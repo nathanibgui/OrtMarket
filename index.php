8e015e45e4554ca2a64b366f8d0ec8fe301ee34a
Nathan55555 <Nathansroussi@gmail.com>
2023-02-08 16:32:28 +0100

    V0

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include('vue/navbar.php') ?>
+    <div class="container mt-5">
+      <h1 class="text-center">ORT MARKET</h1>
+      <p class="text-center">Bienvenue sur la plateforme de commande de snacks pour les élèves</p>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+   
+    <div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="card">
+        <img src="https://boutique.sofratel.fr/provins/wp-content/uploads/sites/28/2021/12/kinderbueno.jpg" class="card-img-top" alt="Snack 1">
+        <div class="card-body">
+          <h5 class="card-title">Snack 1</h5>
+          <p class="card-text">Description du snack 1</p>
+          <a href="#" class="btn btn-primary">Ajouter au panier</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="card">
+      <img src="https://boutique.sofratel.fr/provins/wp-content/uploads/sites/28/2021/12/kinderbueno.jpg" class="card-img-top" alt="Snack 1">
+        <div class="card-body">
+          <h5 class="card-title">Snack 2</h5>
+          <p class="card-text">Description du snack 2</p>
+          <a href="#" class="btn btn-primary">Ajouter au panier</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="card">
+      <img src="https://boutique.sofratel.fr/provins/wp-content/uploads/sites/28/2021/12/kinderbueno.jpg" class="card-img-top" alt="Snack 1">
+        <div class="card-body">
+          <h5 class="card-title">Snack 3</h5>
+          <p class="card-text">Description du snack 3</p>
+          <a href="#" class="btn btn-primary">Ajouter au panier</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ vue/navbar.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>ORT MARKET</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">ORT MARKET</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Commandes</a>
+          </li>
+          <li class="nav-item active" >
+            <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+</svg></a>
+          </li>
+          <li class="nav-item active" >
+            <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+  <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
+</svg></a>
+          </li>
+        </ul>
+      </div>
+    </nav>

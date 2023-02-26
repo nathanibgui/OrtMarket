388edca4304da9e2980e00042a527cec3e12dd3a
Nathan55555 <77548288+Nathan55555@users.noreply.github.com>
2023-02-26 14:10:00 +0100

    Update db_shop_online.sql

--- db_shop_online.sql
+++ db_shop_online.sql
@@ -7,9 +7,6 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-CREATE DATABASE `Shop_online` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `Shop_online`;
-
 DROP TABLE IF EXISTS `Adresse`;
 CREATE TABLE `Adresse` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -23,6 +20,9 @@ CREATE TABLE `Adresse` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `Adresse` (`id`, `id_users`, `Title`, `Pays`, `Ville`, `Code_postal`, `Rue`, `N°`) VALUES
+(1,	1,	'CASIP',	'France',	'Paris',	75020,	'Rue Pali Kao',	8),
+(2,	1,	'CASIP',	'France',	'Paris',	75020,	'Rue Pali Kao',	8);
 
 DROP TABLE IF EXISTS `Articles`;
 CREATE TABLE `Articles` (
@@ -38,6 +38,9 @@ CREATE TABLE `Articles` (
   CONSTRAINT `Articles_ibfk_1` FOREIGN KEY (`id_Catégories`) REFERENCES `Catégories` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `Articles` (`id`, `Title`, `Description`, `Date_ajout`, `id_Catégories`, `picture`, `qtn`) VALUES
+(7,	'snikker',	'snikkers',	'2026-02-23 00:00:00',	NULL,	'https://th.bing.com/th/id/OIP._dkpUe6xGUCGSpvpHrc-7QHaHa?w=203',	'63'),
+(8,	'Kinder Bueno',	'Kinder',	'2026-02-23 00:00:00',	NULL,	'https://prcttrading.com/wp-content/uploads/2021/05/kinder-bueno-1.jpg',	'4');
 
 DROP TABLE IF EXISTS `Catégories`;
 CREATE TABLE `Catégories` (
@@ -47,6 +50,11 @@ CREATE TABLE `Catégories` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `Catégories` (`id`, `Title`, `Description`) VALUES
+(1,	'Ordinateurs',	'blabla'),
+(2,	'Téléphones',	NULL),
+(3,	'Accessoires Ordinateurs',	'\"Souries clavier ...\"'),
+(4,	'Licences',	'Windows 10,Office etc...');
 
 DROP TABLE IF EXISTS `commande`;
 CREATE TABLE `commande` (
@@ -58,6 +66,12 @@ CREATE TABLE `commande` (
   CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `Users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `commande` (`id_commande`, `date_commande`, `id_client`) VALUES
+(51,	'2023-02-26',	2),
+(52,	'2023-02-26',	2),
+(53,	'2023-02-26',	2),
+(54,	'2023-02-26',	2),
+(55,	'2023-02-26',	1);
 
 DROP TABLE IF EXISTS `ligne_commande`;
 CREATE TABLE `ligne_commande` (
@@ -71,6 +85,15 @@ CREATE TABLE `ligne_commande` (
   CONSTRAINT `ligne_commande_ibfk_2` FOREIGN KEY (`id_produit`) REFERENCES `Articles` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `ligne_commande` (`id_ligne`, `quantite`, `id_commande`, `id_produit`) VALUES
+(33,	1,	51,	7),
+(34,	2,	52,	7),
+(35,	2,	53,	8),
+(36,	2,	53,	7),
+(37,	2,	54,	8),
+(38,	2,	54,	7),
+(39,	2,	55,	8),
+(40,	2,	55,	7);
 
 DROP TABLE IF EXISTS `Users`;
 CREATE TABLE `Users` (
@@ -86,6 +109,7 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `Users` (`id`, `cat`, `Nom`, `Prenom`, `Mail`, `Login`, `Password`, `Date_Creation`) VALUES
+(1,	'root',	'Sroussi',	'Nathan',	'Nathansroussi@gmail.com',	'Nat.5_',	'root',	'2022-11-23 19:16:23'),
 (2,	'root',	'demo',	'demo',	'demo@demo.fr',	'demo',	'demo',	NULL);
 
--- 2023-02-26 09:11:11
+-- 2023-02-26 13:09:30
